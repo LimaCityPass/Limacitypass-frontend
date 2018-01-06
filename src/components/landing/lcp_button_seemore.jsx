489c3de462b501cac0e8ed709f653add9c3c9a90
marianoriega <maria.noriega@utec.edu.pe>
2018-01-06 01:23:32 -0500

    button see more added with movement and link

--- src/components/landing/lcp_button_seemore.jsx
+++ src/components/landing/lcp_button_seemore.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-
+import "./styles/ButtonSeeMore.css";
+import SeeMoreImage from '../../assets/seemore-button.svg'
 
 class ButtonSeeMore extends Component {
     render() {
         return (
-            <div>
-
+            <div className='movementimg cursor-pointer'>
+                <img src={SeeMoreImage} className='seemoreimg'/>
             </div>
         );
     }
